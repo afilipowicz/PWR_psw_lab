5ba909072995ed5aa23f264f3a0fc725a343a38c
Andrzej Filipowicz <andrzej.filipowicz@droidsonroids.pl>
2016-11-17 07:00:47 +0100

    więcej funkcji

---
+++ CSS_2, JS/funkcje.js
@@ -0,0 +1,28 @@
+function addZainteresowaniaIHobby() {
+  "use strict";
+  document.getElementById("Dodaj zawartosc").hidden = "";
+  document.getElementById("Zainteresowania i hobby").innerHTML = "Zainteresowania i hobby";
+}
+
+function insertHobbyText() {
+  var text = prompt("Wprowadz text", "text");
+  if(text != null) {
+    document.getElementById("HobbyParagraph").innerHTML = text;
+  }
+}
+
+function displayAlertAfterRandomTime() {
+  var time = Math.floor((Math.random() * 15) + 5) * 1000;
+  setTimeout(function () {
+    if (window.confirm('Widzę, że spędziłeś na mojej stronie już trochę czasu. Może chcesz mnie zatrudnić?')) {
+      window.prompt("Zostaw swój email, skontaktuję się do dwóch dni.");
+    } else {
+      window.alert("Nic nie szkodzi, miłego dnia ;)");
+    }
+  }, time);
+}
+
+function changeImageSource() {
+  var newSrc = window.prompt("Podaj nowy link do zdjęcia")
+  document.getElementById("profilePhoto").src = newSrc
+}
