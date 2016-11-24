52e173f96811601c408ebfd17ecc70f07ca23dbd
Andrzej Filipowicz <andrzej.filipowicz@droidsonroids.pl>
2016-11-24 07:43:28 +0100

    dom, reszta poprawiona

---
+++ CSS_2, JS/funkcje-kontakt.js
@@ -0,0 +1,25 @@
+function emailFocus() {
+  var emailInput = document.forms.item(0).elements.item(0);
+  emailInput.value = "";
+}
+
+function emailBlur() {
+  var emailInput = document.getElementsByTagName("input")[0];
+  if (emailInput.value == "") {
+    emailInput.value = "john@appleseed.com";
+  }
+}
+
+function formBlur() {
+  var form = document.forms[0];
+  if (confirm("wyslij wiadomosc?")) {
+    form.submit();
+  }
+}
+
+function keyPressedInInput(event) {
+    var key = event.keyCode;
+    if (key == 67 && event.shiftKey) {
+      document.forms.item(0).reset();
+    }
+}
--- CSS_2, JS/funkcje.js
+++ CSS_2, JS/funkcje.js
@@ -12,7 +12,7 @@ function insertHobbyText() {
 }
 
 function displayAlertAfterRandomTime() {
-  var time = Math.floor((Math.random() * 15) + 5) * 1000;
+  var time = Math.floor((Math.random() * 10) + 50) * 1000;
   setTimeout(function () {
     if (window.confirm('Widzę, że spędziłeś na mojej stronie już trochę czasu. Może chcesz mnie zatrudnić?')) {
       window.prompt("Zostaw swój email, skontaktuję się do dwóch dni.");
@@ -27,14 +27,30 @@ function changeImageSource() {
   document.getElementById("profilePhoto").src = newSrc;
 }
 
-function addParagraph(){
+function addParagraph() {
 	var par = document.createElement("P");
-    var br = document.createElement("br");
+  var br = document.createElement("br");
 	par.appendChild(br);
 	document.getElementById("header").appendChild(par);
 }
 
-function changeBackgroundColour(){
+function removeParagraph() {
+	var myH = document.getElementById("header");
+	myH.removeChild(myH.childNodes[1]);
+}
+
+function changeBackgroundColour() {
 	var inputColor = prompt("Wprowadz nazwe koloru tla dla tej strony");
 	document.getElementById("header").setAttribute("style", "background-color: " + inputColor);
+}
+
+function openAppropriateWebsite(item) {
+  switch (item.innerHTML) {
+    case "Swift":
+      window.open("https://swift.org");
+      break;
+    default:
+      window.open("https://github.com/afilipowicz");
+      break;
+  }
 }
