001a54a8402ea5295e35293e88447166da0cff41
Kuba <kububm@gmail.com>
2016-12-04 18:59:15 +0100

    Dodaje troche php

---
+++ CSS_2, JS/form.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>Form Validation</title>
+	</head>
+	<body>
+		<?php
+			$email = $_POST["emailInput"];
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				$emailErr = "Invalid email format"; 
+				print($emailErr);
+			}
+			?>
+		<p>Wybrany email: <?php print($_POST["emailInput"]); ?>
+		<p>Wiadomość: <?php print($_POST["textArea"]); ?>
+		<p>Kopia: <?php print($_POST["selectInput"]); ?>
+	</body>
+</html>
